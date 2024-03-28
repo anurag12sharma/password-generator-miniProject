5ae8fc1104b5fc77bce715d2213f413d31721d4f
Anurag <anurag2002sharma@gmail.com>
2024-03-29 04:03:51 +0530

    added all the functionalities

--- script.js
+++ script.js
@@ -8,12 +8,18 @@ const indicator = document.querySelector("[indicator]");
 const symbols = '~`!@#$%^&*()_-+=[{}]\|;:",<.>/?';
 
 const upperCheck = document.querySelector("#upper");
-const lowerCheck = document.querySelector("lower");
+const lowerCheck = document.querySelector("#lower");
 const numberCheck = document.querySelector("#numbers");
 const symbolCheck = document.querySelector("#symbols");
 
 const passwordDisplay = document.querySelector(".display");
+const copyBtn = document.querySelector("[copyBtn]");
 const copyMsg = document.querySelector("[copyMsg]");
+
+let count = 0;
+const allCheckbox = document.querySelectorAll("input[type=checkbox]");
+
+const generateBtn = document.querySelector("[generate-btn]");
 
 handleSlider();
 // sets the password length
@@ -42,7 +48,7 @@ function getUpperCase(){
     return String.fromCharCode(getRandomInteger(65,91));
 }
 
-function generateSymbol(){
+function getSymbol(){
     const randNum = getRandomInteger(0,symbols.length);
     return symbols.charAt(randNum);
 }
@@ -79,5 +85,85 @@ async function copyContent(){
         copyMsg.innerText = "failed";
     }
 
-    copyMsg.classList.add("active");
+    copyMsg.classList.add("active"); 
+
+    setTimeout(()=>{
+        copyMsg.classList.remove("active");
+    },2000);
 }
+
+slider.addEventListener('input',(e)=>{
+    passwordLength = e.target.value;
+    handleSlider();
+})
+
+copyBtn.addEventListener('click',()=>{
+    if(passwordDisplay.value)
+        copyContent();
+})
+
+function countChecks(){
+    count = 0;
+    allCheckbox.forEach((checkbox)=>{
+        if(checkbox.checked)
+            count++;
+    });
+
+    if(passwordLength<count){
+        passwordLength = count;
+        handleSlider();
+    }
+}
+
+allCheckbox.forEach((checkbox)=>{
+    checkbox.addEventListener('change',countChecks);
+})
+
+function shuffle(array){
+    for(let i=array.length-1; i>0; i--){
+        const j = Math.floor(Math.random()*(i+1));
+        const temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    let str = "";
+    array.forEach((val)=>{str+=val});
+    return str;
+}
+
+generateBtn.addEventListener('click',()=>{
+    if (count==0)
+        return;
+    
+    password = "";
+
+    let funcArr = [];
+
+    if(upperCheck.checked)
+        funcArr.push(getUpperCase);
+    
+    if(lowerCheck.checked)
+        funcArr.push(getLowerCase); 
+    
+    if(symbolCheck.checked)
+        funcArr.push(getSymbol);
+    
+    if(numberCheck.checked)
+        funcArr.push(getRandomNumber);
+
+    for(let i=0; i<funcArr.length; i++){
+        password += funcArr[i]();
+    }
+
+    for(let i=0; i<passwordLength-funcArr.length; i++){
+        let ind = getRandomInteger(0,funcArr.length);
+        password += funcArr[ind]();
+    }
+
+    password = shuffle(Array.from(password));
+
+    passwordDisplay.value = password;
+
+    calStrength();
+
+});
